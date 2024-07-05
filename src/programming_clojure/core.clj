8551929b4bfb9859d12474338a3c1378a91cb99a
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-07-05 11:55:38 +0530

    Check if a string is blank

--- src/programming_clojure/core.clj
+++ src/programming_clojure/core.clj
@@ -1,9 +1,13 @@
 (ns programming-clojure.core
-  (:require [programming-clojure.demo :refer [fibinocci-series]])
+  (:require [programming-clojure.demo :refer [fibinocci-series
+                                              is-string-blank?]])
   (:gen-class))
+
 
 (defn -main
   "main"
   [& args]
   (println "Created fibinocci series number using function programming style")
-  (println (fibinocci-series 10)))
+  (println (fibinocci-series 10))
+  (println "Checks if this string is blank \"    \" : " (is-string-blank? "   "))
+  (println "Check if this string is blanck \"this\" : " (is-string-blank? "this")))
--- src/programming_clojure/demo.clj
+++ src/programming_clojure/demo.clj
@@ -1,9 +1,17 @@
 (ns programming-clojure.demo
   (:gen-class))
 
+;; Some useful functions in clojure
+
+
 (defn fibinocci-series
   "Generates a fibinocci series starting from 0 till n"
   [n]
   (->> (iterate (fn [[a b]] [b (+ a b)]) [0 1])
        (map first)
        (take n)))
+
+
+(defn is-string-blank?
+  [str]
+  (every? #(Character/isWhitespace %) str))
